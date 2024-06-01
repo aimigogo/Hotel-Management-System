7e581108c8406bfd2dff0f0ed37030fb162ff9aa
aimigogo <aimiliagogolou5@gmail.com>
2024-06-01 18:21:49 +0300

    deleted some comments and made the icons change if they are active

--- hms-frontend/src/components/Sidebar.js
+++ hms-frontend/src/components/Sidebar.js
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
 import './Sidebar.css';
 import { ReactComponent as Logo } from '../assets/Logo.svg';
-//import {ReactComponent as DashboardIcon } from '../assets/Inactive/dashboard.svg';
+import {ReactComponent as DashboardIcon } from '../assets/Inactive/dashboard.svg';
 import {ReactComponent as DashboardIconActive } from '../assets/Active/dashboard.svg';
 import {ReactComponent as RoomIcon } from '../assets/Inactive/hotel.svg';
-//import {ReactComponent as RoomIconActive } from '../assets/Active/hotel_active.svg';
+import {ReactComponent as RoomIconActive } from '../assets/Active/hotel.svg';
 import {ReactComponent as AccountIcon } from '../assets/Inactive/account_balance.svg';
-//import {ReactComponent as AccountIconActive } from '../assets/Active/account_balance_active.svg';
+import {ReactComponent as AccountIconActive } from '../assets/Active/account_balance.svg';
 import {ReactComponent as UsersIcon } from '../assets/Inactive/people.svg';
-//import {ReactComponent as UsersIconActive } from '../assets/Active/people_Active.svg';
+import {ReactComponent as UsersIconActive } from '../assets/Active/people.svg';
 import {ReactComponent as SettingsIcon } from '../assets/Inactive/settings.svg';
-//import {ReactComponent as SettingsIconActive } from '../assets/Active/settings_active.svg';
+import {ReactComponent as SettingsIconActive } from '../assets/Active/settings.svg';
 import {ReactComponent as ExitIcon } from '../assets/Inactive/exit_to_app.svg';
-//import {ReactComponent as ExitIconActive } from '../assets/Active/exit_to_app_active.svg';
+import {ReactComponent as ExitIconActive } from '../assets/Active/exit_to_app.svg';
 
 
 
@@ -31,37 +31,38 @@ export default function Sidebar() {
                 <li className={`sidebar-item ${activeItem === 'dashboard'? 'active':''}`}
                     onClick={()=>handleItemClick('dashboard')}>
                     <a href="#dashboard">
-                        <DashboardIconActive classname="sidebar-icon" />
+                        {activeItem === 'dashboard' ? <DashboardIconActive className="sidebar-icon" />
+                            : <DashboardIcon className="sidebar-icon" />}
                         Dashboard</a></li>
                 <li className={`sidebar-item ${activeItem === 'rooms'? 'active':''}`}
                     onClick={()=>handleItemClick('rooms')}>
                     <a href="#rooms">
-                        <RoomIcon classname="sidebar-icon"/>
-                        {/*{`sidebar-icon ${activeItem ==='rooms' ? RoomIconActive:RoomIcon}`} />*/}
+                        {activeItem === 'rooms' ? <RoomIconActive className="sidebar-icon" />
+                            : <RoomIcon className="sidebar-icon" />}
                         Rooms</a></li>
                 <li className={`sidebar-item ${activeItem === 'accounts'? 'active':''}`}
                     onClick={()=>handleItemClick('accounts')}>
                     <a href="#accounts">
-                        <AccountIcon classname="sidebar-icon"/>
-                        {/*{`sidebar-icon ${activeItem ==='accounts' ? AccountIconActive:AccountIcon}`} />*/}
+                        {activeItem === 'accounts' ? <AccountIconActive className="sidebar-icon" />
+                            : <AccountIcon className="sidebar-icon" />}
                         Accounts</a></li>
                 <li className={`sidebar-item ${activeItem === 'users'? 'active':''}`}
                     onClick={()=>handleItemClick('users')}>
                     <a href="#users">
-                        <UsersIcon classname="sidebar-icon"/>
-                        {/*{`sidebar-icon ${activeItem ==='users' ? UsersIconActive:UsersIcon}`} />*/}
+                        {activeItem === 'users' ? <UsersIconActive className="sidebar-icon" />
+                            : <UsersIcon className="sidebar-icon" />}
                         Users</a></li>
                 <li className={`sidebar-item ${activeItem === 'settings'? 'active':''}`}
                     onClick={()=>handleItemClick('settings')}>
                     <a href="#settings">
-                        <SettingsIcon classname="sidebar-icon"/>
-                        {/*{'sidebar-icon ${activeItem ==='settings' ? SettingsIconActive:SettingsIcon}`} />*/}
+                        {activeItem === 'settings' ? <SettingsIconActive className="sidebar-icon" />
+                            : <SettingsIcon className="sidebar-icon" />}
                         Settings</a></li>
                 <li className={`sidebar-item ${activeItem === 'exit'? 'active':''}`}
                     onClick={()=>handleItemClick('exit')}>
                     <a href="#exit">
-                        <ExitIcon classname="sidebar-icon"/>
-                                      {/*{`sidebar-icon ${activeItem ==='exit' ? ExitIconActive:ExitIcon}`} />*/}
+                        {activeItem === 'exit' ? <ExitIconActive className="sidebar-icon" />
+                            : <ExitIcon className="sidebar-icon" />}
                         Exit</a></li>
             </ul>
         </div>
