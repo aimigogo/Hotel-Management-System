70a83de9130dca284f3f1391ed713b76274b1141
aimigogo <aimiliagogolou5@gmail.com>
2024-06-03 18:36:45 +0300

    Created HotelManagementSystemApplication.java

---
+++ hms-backend/src/main/java/hms_backend/HotelManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package hms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(HotelManagementSystemApplication.class,args);
+    }
+}
