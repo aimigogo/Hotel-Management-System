ee8f3229bf42d2f9d02c28eb6c4a95fcebf72d44
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:06:37 +0300

    created the room mapper where it takes from roomdto to room and reverse

--- hms-backend/src/main/java/hms_backend/mapper/RoomMapper.java
+++ hms-backend/src/main/java/hms_backend/mapper/RoomMapper.java
@@ -1,4 +1,25 @@
 package hms_backend.mapper;
 
+import hms_backend.dto.RoomDto;
+import hms_backend.model.Room;
+
 public interface RoomMapper {
+
+    public static RoomDto mapToRoomDto(Room room){
+        return new RoomDto(
+                room.getId(),
+                room.getStatus(),
+                room.getAmenities(),
+                room.getType()
+        );
+    }
+
+    public static Room mapToRoom(RoomDto roomDto){
+        return new Room(
+                roomDto.getId(),
+                roomDto.getStatus(),
+                roomDto.getAmenities(),
+                roomDto.getType()
+        );
+    }
 }
