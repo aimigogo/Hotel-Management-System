17ee5685ac3f8dbf17e2779d95e4131963fba9c8
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:07:22 +0300

    made the RoomRepository extends the jpaRepository

--- hms-backend/src/main/java/hms_backend/repository/RoomRepository.java
+++ hms-backend/src/main/java/hms_backend/repository/RoomRepository.java
@@ -1,4 +1,10 @@
 package hms_backend.repository;
 
-public interface RoomRepository {
+import hms_backend.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+@Repository
+@Transactional
+public interface RoomRepository extends JpaRepository<Room,Long> {
 }
