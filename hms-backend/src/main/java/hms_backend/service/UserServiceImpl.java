1a8af7ed9bdeed9810ea472686de2e4fa88b79dc
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:17:43 +0300

    implemented the creation of user and added annotations

--- hms-backend/src/main/java/hms_backend/service/UserServiceImpl.java
+++ hms-backend/src/main/java/hms_backend/service/UserServiceImpl.java
@@ -1,4 +1,25 @@
 package hms_backend.service;
 
-public class UserServiceImpl {
+import hms_backend.dto.UserDto;
+import hms_backend.mapper.UserMapper;
+import hms_backend.model.User;
+import hms_backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        User user= UserMapper.mapToUser(userDto);
+        User savedUser= userRepository.save(user);
+        return UserMapper.mapToUserDto(savedUser);
+    }
+
+
+
 }
