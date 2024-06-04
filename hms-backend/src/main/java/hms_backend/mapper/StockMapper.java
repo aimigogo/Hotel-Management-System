647b84263de5d47cc5ed4be49d6d84650edefc1b
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:13:22 +0300

    created the StockMapper where it takes from stockdto to stock and reverse

--- hms-backend/src/main/java/hms_backend/mapper/StockMapper.java
+++ hms-backend/src/main/java/hms_backend/mapper/StockMapper.java
@@ -1,4 +1,23 @@
 package hms_backend.mapper;
 
+import hms_backend.dto.StockDto;
+import hms_backend.model.Stock;
+
 public interface StockMapper {
+
+    public static StockDto mapToStockDto(Stock stock){
+        return new StockDto(
+                stock.getId(),
+                stock.getName(),
+                stock.getQuantity()
+        );
+    }
+
+    public static Stock mapToStock(StockDto stockDto){
+        return new Stock(
+                stockDto.getId(),
+                stockDto.getName(),
+                stockDto.getQuantity()
+        );
+    }
 }
