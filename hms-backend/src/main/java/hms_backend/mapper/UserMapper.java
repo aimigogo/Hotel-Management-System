3cd74e8fb21e9ba62811108888eacd4f8034dc82
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:14:05 +0300

    created the UserMapper where it takes from userdto to user and reverse

--- hms-backend/src/main/java/hms_backend/mapper/UserMapper.java
+++ hms-backend/src/main/java/hms_backend/mapper/UserMapper.java
@@ -1,4 +1,33 @@
 package hms_backend.mapper;
 
+import hms_backend.dto.UserDto;
+import hms_backend.model.User;
+
 public interface UserMapper {
+
+    public static UserDto mapToUserDto(User user){
+        return new UserDto(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+
+    public static User mapToUser(UserDto userDto){
+        return new User(
+                userDto.getId(),
+                userDto.getFirstname(),
+                userDto.getLastName(),
+                userDto.getEmail(),
+                userDto.getUsername(),
+                userDto.getPassword(),
+                userDto.getRole()
+        );
+    }
+
+
 }
