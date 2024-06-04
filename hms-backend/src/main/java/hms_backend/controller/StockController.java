672f4388fb1473edbdffd7e17de1aa2cab5132ab
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:11:14 +0300

    Created the api for creating a stock item and added the anontetions

--- hms-backend/src/main/java/hms_backend/controller/StockController.java
+++ hms-backend/src/main/java/hms_backend/controller/StockController.java
@@ -1,4 +1,23 @@
 package hms_backend.controller;
 
+import hms_backend.dto.StockDto;
+import hms_backend.service.StockService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/stocks")
 public class StockController {
+    private StockService stockService;
+    @PostMapping
+    public ResponseEntity<StockDto> createStockItem(@RequestBody StockDto stockDto){
+        StockDto savedStockItem=stockService.createStockItem(stockDto);
+        return new ResponseEntity<>(savedStockItem, HttpStatus.CREATED);
+    }
 }
