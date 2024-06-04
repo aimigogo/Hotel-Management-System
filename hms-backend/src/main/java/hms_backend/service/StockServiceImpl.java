d0847cb3368f73b15fb31caa1ff0f743a216702f
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:17:27 +0300

    implemented the creation of stock item and added annotations

--- hms-backend/src/main/java/hms_backend/service/StockServiceImpl.java
+++ hms-backend/src/main/java/hms_backend/service/StockServiceImpl.java
@@ -1,4 +1,22 @@
 package hms_backend.service;
 
-public class StockServiceImpl {
+
+import hms_backend.dto.StockDto;
+import hms_backend.mapper.StockMapper;
+import hms_backend.model.Stock;
+import hms_backend.repository.StockRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class StockServiceImpl implements StockService{
+
+    private StockRepository stockRepository;
+    @Override
+    public StockDto createStockItem(StockDto stockDto) {
+        Stock stock= StockMapper.mapToStock(stockDto);
+        Stock savedStockItem=stockRepository.save(stock);
+        return StockMapper.mapToStockDto(savedStockItem);
+    }
 }
