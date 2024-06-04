f431f98b4e67b6e2689246381e68454eda725442
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:19:07 +0300

    Changed from serial to simple id added username and changed from name to firstname

--- hms-backend/src/main/java/hms_backend/model/User.java
+++ hms-backend/src/main/java/hms_backend/model/User.java
@@ -9,8 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.Serial;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,23 +16,30 @@ import java.io.Serial;
 @Entity
 @Table(name="users",indexes = {@Index(columnList = "email")})
 @SequenceGenerator(name = "idGenerator",sequenceName = "USER_SEQ", initialValue = 1,allocationSize = 1)
-
 public class User{
-    @Serial
-    private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idGenerator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "idGenerator")
     @Column(updatable = false)
     private Long id;
+
     @Column
-    private String name;
-    @NotNull(message = "Email address cannot be null")
+    private String firstName;
+
+    @Column
+    private String lastName;
+
     @Email
     @Column(length = 50, nullable = false, unique = true)
     private String email;
+
+    @Column
+    @NotNull
+    private String username;
     @Column
     @NotNull
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(length =10,nullable = false )
     private Role role;
