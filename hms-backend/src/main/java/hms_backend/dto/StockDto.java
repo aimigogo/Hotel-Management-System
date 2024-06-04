c446dac3e914269c9664976f5d6c1f0363e45a89
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:12:22 +0300

    Added the StockDto

--- hms-backend/src/main/java/hms_backend/dto/StockDto.java
+++ hms-backend/src/main/java/hms_backend/dto/StockDto.java
@@ -1,4 +1,19 @@
 package hms_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockDto {
+
+    private Long id;
+
+    private String name;
+
+    private Integer quantity;
 }
