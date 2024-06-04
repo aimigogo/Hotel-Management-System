5b962e8770f615164d71635cde182638cf4aaa8c
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:03:12 +0300

    Created the api for room creation

--- hms-backend/src/main/java/hms_backend/controller/RoomController.java
+++ hms-backend/src/main/java/hms_backend/controller/RoomController.java
@@ -1,4 +1,26 @@
 package hms_backend.controller;
 
+import hms_backend.dto.RoomDto;
+import hms_backend.service.RoomService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomController {
+
+    private RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<RoomDto> createRoom(@RequestBody RoomDto roomDto){
+        RoomDto savedRoom=roomService.createRoom(roomDto);
+        return new ResponseEntity<>(savedRoom, HttpStatus.CREATED);
+    }
+
 }
