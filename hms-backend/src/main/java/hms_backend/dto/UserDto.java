4f95177f4c678a82f8039ff2f71654578be35f8d
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:20:10 +0300

    created user dto

--- hms-backend/src/main/java/hms_backend/dto/UserDto.java
+++ hms-backend/src/main/java/hms_backend/dto/UserDto.java
@@ -1,4 +1,28 @@
 package hms_backend.dto;
 
+import hms_backend.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    private Long id;
+
+    private String firstname;
+
+    private String lastName;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    private Role role;
 }
