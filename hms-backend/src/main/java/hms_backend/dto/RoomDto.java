a14aee495a6cf6aaa5ab2e88b31ebcb4e2d6fd8f
aimigogo <aimiliagogolou5@gmail.com>
2024-06-04 19:03:34 +0300

    Added the dto for room

--- hms-backend/src/main/java/hms_backend/dto/RoomDto.java
+++ hms-backend/src/main/java/hms_backend/dto/RoomDto.java
@@ -1,4 +1,24 @@
 package hms_backend.dto;
 
+import hms_backend.model.enums.Amenities;
+import hms_backend.model.enums.Status;
+import hms_backend.model.enums.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomDto {
+
+    private Long id;
+
+    private Status status;
+
+    private Amenities amenities;
+
+    private Type type;
 }
