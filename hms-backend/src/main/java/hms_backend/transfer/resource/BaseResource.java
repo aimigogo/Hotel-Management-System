c8e43a69b5f0ef43fb0e8e632c088157320c9acc
aimigogo <aimiliagogolou5@gmail.com>
2024-06-08 18:29:16 +0300

    created BaseResource.java

---
+++ hms-backend/src/main/java/hms_backend/transfer/resource/BaseResource.java
@@ -0,0 +1,13 @@
+package hms_backend.transfer.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@ToString
+public class BaseResource implements Serializable {
+    private Long id;
+}
