29867780da485e9ee1e06b3659644fd83f1509e9
aimigogo <aimiliagogolou5@gmail.com>
2024-06-08 18:33:50 +0300

    Almost completed StockController and made it extends BaseController

--- hms-backend/src/main/java/hms_backend/controller/StockController.java
+++ hms-backend/src/main/java/hms_backend/controller/StockController.java
@@ -1,23 +1,44 @@
 package hms_backend.controller;
 
-import hms_backend.dto.StockDto;
+
+import hms_backend.mapper.BaseMapper;
+import hms_backend.mapper.StockMapper;
+import hms_backend.model.Stock;
+import hms_backend.service.BaseService;
 import hms_backend.service.StockService;
+import hms_backend.transfer.ApiResponse;
+import hms_backend.transfer.resource.StockResource;
 import lombok.AllArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/stocks")
-public class StockController {
+public class StockController extends BaseController<Stock, StockResource> {
     private StockService stockService;
-    @PostMapping
-    public ResponseEntity<StockDto> createStockItem(@RequestBody StockDto stockDto){
-        StockDto savedStockItem=stockService.createStockItem(stockDto);
-        return new ResponseEntity<>(savedStockItem, HttpStatus.CREATED);
+    private final StockMapper stockMapper;
+
+    @Override
+    protected BaseService<Stock, Long> getBaseService() {
+        return stockService;
     }
+
+    @Override
+    protected BaseMapper<Stock, StockResource> getMapper() {
+        return stockMapper;
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<ApiResponse<StockResource>> getItemByName(@PathVariable("name")final String name){
+        return ResponseEntity.ok(
+                ApiResponse.<StockResource>builder()
+                        .data(stockMapper.toResource(stockService.getStockByName(name)))
+                        .build());
+    }
+
+
 }
