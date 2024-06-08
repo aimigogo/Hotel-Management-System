5cd842941a2989c7872e539e00a393241fff3d05
aimigogo <aimiliagogolou5@gmail.com>
2024-06-08 18:29:04 +0300

    created BaseModel.java

---
+++ hms-backend/src/main/java/hms_backend/model/BaseModel.java
@@ -0,0 +1,22 @@
+package hms_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@MappedSuperclass
+public class BaseModel implements Serializable {
+
+    private static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "idGenerator")
+    @Column(updatable = false)
+    private Long id;
+
+}
