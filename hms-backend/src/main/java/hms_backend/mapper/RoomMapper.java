38ad05fb0cd92d5cbefa5db017697c7466d69a98
aimigogo <aimiliagogolou5@gmail.com>
2024-06-08 18:36:11 +0300

    Made roomMapper extend baseMapper

--- hms-backend/src/main/java/hms_backend/mapper/RoomMapper.java
+++ hms-backend/src/main/java/hms_backend/mapper/RoomMapper.java
@@ -1,25 +1,11 @@
 package hms_backend.mapper;
 
-import hms_backend.dto.RoomDto;
+
 import hms_backend.model.Room;
+import hms_backend.transfer.resource.RoomResource;
+import org.mapstruct.Mapper;
 
-public interface RoomMapper {
+@Mapper(componentModel = "spring",config = IgnoreUnmappedMapperConfig.class)
+public interface RoomMapper extends BaseMapper<Room, RoomResource> {
 
-    public static RoomDto mapToRoomDto(Room room){
-        return new RoomDto(
-                room.getId(),
-                room.getStatus(),
-                room.getAmenities(),
-                room.getType()
-        );
-    }
-
-    public static Room mapToRoom(RoomDto roomDto){
-        return new Room(
-                roomDto.getId(),
-                roomDto.getStatus(),
-                roomDto.getAmenities(),
-                roomDto.getType()
-        );
-    }
 }
