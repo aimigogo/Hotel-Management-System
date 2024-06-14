2501c736cf39167233021370a45fd404af9dff4f
aimigogo <aimiliagogolou5@gmail.com>
2024-06-14 20:14:50 +0300

    Created the EmployeeDashboard.js and the css file

---
+++ hms-frontend/src/components/EmployeeDashboard.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from "react";
+import Sidebar from "./Sidebar";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+import{listEmployees} from "../Service/EmployeeService";
+import "../css/EmployeeDashboard.css"
+
+const EmployeeDashboard=()=>{
+
+    const [employees,setEmployees] = useState([])
+
+    useEffect(() => {
+        listEmployees().then((response)=>{
+            setEmployees(response.data);
+        }).catch(error=>{
+            console.error(error);
+        })
+    }, []);
+
+    return(
+        <div className="employee-dashboard">
+            <Sidebar/>
+            <div className="dashboard-content">
+                <h2 className='text-center text-white'>Weekly Schedule</h2>
+                <table className='table table-striped table-bordered'>
+                    <thead>
+                    <tr>
+                        <th>Employee Name</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        employees.map(employees=>
+                        <tr key={employees.id}>
+                            <td>{employees.name}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                            <td>{employees.section}{employees.shift}</td>
+                        </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    )
+}
+export default EmployeeDashboard
