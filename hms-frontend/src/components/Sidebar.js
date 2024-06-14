763eee99955bcc60502bff34d9f33fe861515e09
aimigogo <aimiliagogolou5@gmail.com>
2024-06-14 20:23:47 +0300

    Added useUser where it takes you back to the correct dashboard based on the role you have from other pages e.g room page to dashboard

--- hms-frontend/src/components/Sidebar.js
+++ hms-frontend/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from "react-router-dom";
 import '../css/Sidebar.css';
 import { ReactComponent as Logo } from '../assets/Logo.svg';
 import {ReactComponent as DashboardIcon } from '../assets/Inactive/dashboard.svg';
@@ -7,20 +8,21 @@ import {ReactComponent as RoomIcon } from '../assets/Inactive/hotel.svg';
 import {ReactComponent as RoomIconActive } from '../assets/Active/hotel.svg';
 import {ReactComponent as StockIcon } from '../assets/Inactive/store.svg';
 import {ReactComponent as StockIconActive } from '../assets/Active/store.svg';
-import {ReactComponent as UsersIcon } from '../assets/Inactive/people.svg';
-import {ReactComponent as UsersIconActive } from '../assets/Active/people.svg';
 import {ReactComponent as SettingsIcon } from '../assets/Inactive/settings.svg';
 import {ReactComponent as SettingsIconActive } from '../assets/Active/settings.svg';
 import {ReactComponent as ExitIcon } from '../assets/Inactive/exit_to_app.svg';
 import {ReactComponent as ExitIconActive } from '../assets/Active/exit_to_app.svg';
+import {useUser} from "../context/UserRoleContext";
 
 
 
 export default function Sidebar() {
     const [activeItem,setActiveItem]=useState('dashboard');
+    const {userType}=useUser();
 
     const handleItemClick=(item)=>{
         setActiveItem(item);
+
     }
     return (
         <div className="sidebar">
@@ -30,40 +32,42 @@ export default function Sidebar() {
             <ul className="sidebar-links">
                 <li className={`sidebar-item ${activeItem === 'dashboard'? 'active':''}`}
                     onClick={()=>handleItemClick('dashboard')}>
-                    <a href="#dashboard">
+                    <Link to={userType === 'ADMIN'?"/AdminDashboard":"/EmployeeDashboard"} >
                         {activeItem === 'dashboard' ? <DashboardIconActive className="sidebar-icon" />
                             : <DashboardIcon className="sidebar-icon" />}
-                        Dashboard</a></li>
-                <li className={`sidebar-item ${activeItem === 'rooms'? 'active':''}`}
-                    onClick={()=>handleItemClick('rooms')}>
-                    <a href="#rooms">
+                        Dashboard</Link></li>
+                <li className={`sidebar-item ${activeItem === 'rooms' ? 'active' : ''}`}
+                    onClick={() => handleItemClick('rooms')}>
+                    <Link to="/RoomPage">
                         {activeItem === 'rooms' ? <RoomIconActive className="sidebar-icon" />
                             : <RoomIcon className="sidebar-icon" />}
-                        Rooms</a></li>
+                        Rooms
+                    </Link>
+                </li>
                 <li className={`sidebar-item ${activeItem === 'stock'? 'active':''}`}
                     onClick={()=>handleItemClick('stock')}>
-                    <a href="#stock">
+                    <Link to="/StockPage">
                         {activeItem === 'stock' ? <StockIconActive className="sidebar-icon" />
                             : <StockIcon className="sidebar-icon" />}
-                        Stock</a></li>
-                <li className={`sidebar-item ${activeItem === 'users'? 'active':''}`}
-                    onClick={()=>handleItemClick('users')}>
-                    <a href="#users">
-                        {activeItem === 'users' ? <UsersIconActive className="sidebar-icon" />
-                            : <UsersIcon className="sidebar-icon" />}
-                        Users</a></li>
+                        Stock
+                </Link>
+            </li>
                 <li className={`sidebar-item ${activeItem === 'settings'? 'active':''}`}
                     onClick={()=>handleItemClick('settings')}>
-                    <a href="#settings">
+                    <Link to="/SettingsPage">
                         {activeItem === 'settings' ? <SettingsIconActive className="sidebar-icon" />
                             : <SettingsIcon className="sidebar-icon" />}
-                        Settings</a></li>
+                        Settings
+                    </Link>
+                </li>
                 <li className={`sidebar-item ${activeItem === 'exit'? 'active':''}`}
                     onClick={()=>handleItemClick('exit')}>
-                    <a href="#exit">
+                    <Link to="/Login">
                         {activeItem === 'exit' ? <ExitIconActive className="sidebar-icon" />
                             : <ExitIcon className="sidebar-icon" />}
-                        Exit</a></li>
+                        Exit
+                    </Link>
+                </li>
             </ul>
         </div>
     )
