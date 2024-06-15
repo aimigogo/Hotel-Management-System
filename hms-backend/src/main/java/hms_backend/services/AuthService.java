9cf293067da290704a098ec28d014cda0494ba79
aimigogo <aimiliagogolou5@gmail.com>
2024-06-15 20:37:46 +0300

    added createRoom and stock item service and implemented them

--- hms-backend/src/main/java/hms_backend/services/AuthService.java
+++ hms-backend/src/main/java/hms_backend/services/AuthService.java
@@ -1,8 +1,14 @@
 package hms_backend.services;
 
+import hms_backend.dto.RoomDto;
 import hms_backend.dto.SignupRequest;
+import hms_backend.dto.StockDto;
 import hms_backend.dto.UserDto;
 
 public interface AuthService {
     UserDto createUser(SignupRequest signupRequest);
+
+    RoomDto createRoom(RoomDto roomDto);
+    StockDto createStockItem(StockDto stockDto);
+
 }
--- hms-backend/src/main/java/hms_backend/services/AuthServiceImpl.java
+++ hms-backend/src/main/java/hms_backend/services/AuthServiceImpl.java
@@ -1,9 +1,15 @@
 package hms_backend.services;
 
+import hms_backend.dto.RoomDto;
 import hms_backend.dto.SignupRequest;
+import hms_backend.dto.StockDto;
 import hms_backend.dto.UserDto;
+import hms_backend.entity.Room;
+import hms_backend.entity.Stock;
 import hms_backend.entity.User;
 import hms_backend.entity.enums.UserRole;
+import hms_backend.repository.RoomRepository;
+import hms_backend.repository.StockRepository;
 import hms_backend.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityExistsException;
@@ -13,10 +19,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+import static org.aspectj.apache.bcel.Repository.getRepository;
+
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService{
     private final UserRepository userRepository;
+
+    private final RoomRepository roomRepository;
+
+    private final StockRepository stockRepository;
 
     @PostConstruct
     public void createAdminAccount(){
@@ -47,4 +59,30 @@ public class AuthServiceImpl implements AuthService{
         User createdUser=userRepository.save(user);
         return createdUser.getUserDto();
     }
+    public RoomDto createRoom(RoomDto roomDto){
+        if (roomRepository.findFirstByName(roomDto.getName()).isPresent()){
+            throw new EntityExistsException("Room already exists with name "+roomDto.getName());
+        }
+
+        Room room=new Room();
+        room.setName(roomDto.getName());
+        room.setType(roomDto.getType());
+        room.setPrice(roomDto.getPrice());
+        Room createdRoom=roomRepository.save(room);
+        return createdRoom.getRoomDto();
+    }
+
+    @Override
+    public StockDto createStockItem(StockDto stockDto) {
+
+        if (stockRepository.findFirstByName(stockDto.getName()).isPresent()){
+            throw new EntityExistsException("Stock Item already exists with name"+stockDto.getName());
+        }
+        Stock stock=new Stock();
+        stock.setName(stockDto.getName());
+        stock.setQuantity(stockDto.getQuantity());
+        Stock createdStockItem=stockRepository.save(stock);
+        return createdStockItem.getStockDto();
+    }
+
 }
