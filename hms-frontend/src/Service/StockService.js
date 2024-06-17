79823f971a0e4f0236a312ca6f880de02ef4930b
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:41:21 +0300

    Added correct Api calls

--- hms-frontend/src/Service/StockService.js
+++ hms-frontend/src/Service/StockService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const listStockItems=()=> axios.get('http://localhost:8080/api/auth/stocks');
+export const listStockItems=()=> axios.get('http://localhost:8080/api/stock/stocks');
 
-export const updateStockItem = (stockId, stockData) => axios.put(`http://localhost:8080/api/admin/stockItem/update/${stockId}`, stockData);
+export const updateStockItem = (stockId, stockData) => axios.put(`http://localhost:8080/api/stock/update/${stockId}`, stockData);
 
-export const deleteStockItem = (stockId) => axios.delete(`http://localhost:8080/api/admin/stockItem/delete/${stockId}`);
+export const deleteStockItem = (stockId) => axios.delete(`http://localhost:8080/api/stock/delete/${stockId}`);
