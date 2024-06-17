cc52e69bf1b79b0e9da4cce24c77d0d8b7cb8cd0
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:29:55 +0300

    Added room and stock provider

--- hms-frontend/src/App.js
+++ hms-frontend/src/App.js
@@ -7,30 +7,34 @@ import EmployeeDashboard from "../src/components/EmployeeDashboard";
 import RoomPage from "../src/components/RoomPage";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import StockPage from "./components/StockPage";
-import SettingsPage from "./components/SettingsPage";
-import {UserProvider} from "./context/UserRoleContext";
+import {UserProvider} from "./context/UserContext";
 import AddUserForm from "./components/AddUserForm";
 import AddRoomForm from "./components/AddRoomForm";
 import AddStockItemForm from "./components/AddStockItemForm";
+import {RoomProvider} from "./context/RoomContext";
+import {StockItemProvider} from "./context/StockItemContext";
 
 
 function App() {
     return (
         <UserProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/AdminDashboard" element={<AdminDashboard />} />
-                <Route path="/EmployeeDashboard" element={<EmployeeDashboard />} />
-                <Route path="/RoomPage" element={<RoomPage/>}/>
-                <Route path="/StockPage" element={<StockPage/>}/>
-                <Route path="/SettingsPage" element={<SettingsPage/>}/>
-                <Route path="/AddUserForm" element={<AddUserForm/>}/>
-                <Route path="/AddRoomForm" element={<AddRoomForm/>}/>
-                <Route path="/AddStockItemForm" element={<AddStockItemForm/>}/>
-            </Routes>
-        </Router>
+                <RoomProvider>
+                    <StockItemProvider>
+                        <Router>
+                            <Routes>
+                                <Route path="/" element={<Login />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/AdminDashboard" element={<AdminDashboard />} />
+                                <Route path="/EmployeeDashboard" element={<EmployeeDashboard />} />
+                                <Route path="/RoomPage" element={<RoomPage/>}/>
+                                <Route path="/StockPage" element={<StockPage/>}/>
+                                <Route path="/AddUserForm" element={<AddUserForm/>}/>
+                                <Route path="/AddRoomForm" element={<AddRoomForm/>}/>
+                                <Route path="/AddStockItemForm" element={<AddStockItemForm/>}/>
+                            </Routes>
+                        </Router>
+                    </StockItemProvider>
+                </RoomProvider>
         </UserProvider>
   );
 }
