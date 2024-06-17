9d9901f3871da7d853b74729a32c98aa29d0e69b
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:41:05 +0300

    Added createStockItem,updateStockItem,deleteStockItem and their implementation

--- hms-backend/src/main/java/hms_backend/services/StockService.java
+++ hms-backend/src/main/java/hms_backend/services/StockService.java
@@ -5,5 +5,11 @@ import hms_backend.dto.StockDto;
 import java.util.List;
 
 public interface StockService {
+
+    StockDto createStockItem(StockDto stockDto);
     List<Object> getAllStockItems();
+
+    StockDto updateStockItem(Long id, StockDto stockDto);
+
+    void deleteStockItem(Long id);
 }
--- hms-backend/src/main/java/hms_backend/services/StockServiceImpl.java
+++ hms-backend/src/main/java/hms_backend/services/StockServiceImpl.java
@@ -1,8 +1,12 @@
 package hms_backend.services;
 
 import hms_backend.dto.StockDto;
+import hms_backend.dto.UserDto;
 import hms_backend.entity.Stock;
+import hms_backend.entity.User;
 import hms_backend.repository.StockRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,19 @@ import java.util.stream.Collectors;
 public class StockServiceImpl implements StockService{
 
     private final StockRepository stockRepository;
+
+    @Override
+    public StockDto createStockItem(StockDto stockDto) {
+
+        if (stockRepository.findFirstByName(stockDto.getName()).isPresent()){
+            throw new EntityExistsException("Stock Item already exists with name"+stockDto.getName());
+        }
+        Stock stock=new Stock();
+        stock.setName(stockDto.getName());
+        stock.setQuantity(stockDto.getQuantity());
+        Stock createdStockItem=stockRepository.save(stock);
+        return createdStockItem.getStockDto();
+    }
 
     @Override
     public List<Object> getAllStockItems() {
@@ -29,5 +46,19 @@ public class StockServiceImpl implements StockService{
         stockDto.setName(stock.getName());
         stockDto.setQuantity(stock.getQuantity());
         return stockDto;
+    }
+
+    public StockDto updateStockItem(Long id, StockDto stockDto){
+        Stock stockItem=stockRepository.findById(id)
+                .orElseThrow(()->new EntityNotFoundException("Stock item not found"));
+        stockItem.setName(stockDto.getName());
+        stockItem.setQuantity(stockDto.getQuantity());
+        stockRepository.save(stockItem);
+        return new StockDto();
+
+    }
+
+    public void deleteStockItem(Long id){
+        stockRepository.deleteById(id);
     }
 }
