4dc29eaa9faa2b2a18dcc06d321eab0288ae4db0
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:34:05 +0300

    Added userId

--- hms-frontend/src/Service/EmployeeService.js
+++ hms-frontend/src/Service/EmployeeService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export const listEmployees=()=> axios.get('http://localhost:8080/api/auth/employees');
 
-export const updateEmployee = (userId, userData) => axios.put(`http://localhost:8080/api/auth/employees/update/{id}`, userData);
+export const updateEmployee = (userId, userData) => axios.put(`http://localhost:8080/api/auth/employees/update/${userId}`, userData);
 
-export const deleteEmployee = (userId) => axios.delete(`http://localhost:8080/api/auth/employees/delete/{id}`);
+export const deleteEmployee = (userId) => axios.delete(`http://localhost:8080/api/auth/employees/delete/${userId}`);
