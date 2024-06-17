136955176b78953ff30eb502a79cc81e53336441
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:28:35 +0300

    Added section and shift and the css

--- hms-frontend/src/components/AddUserForm.js
+++ hms-frontend/src/components/AddUserForm.js
@@ -2,29 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import "../css/AddUserForm.css";
 import { useNavigate, useLocation } from "react-router-dom";
+import {useUser} from "../context/UserContext";
 
 const AddUserForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [section, setSection] = useState('');
+    const [shift, setShift] = useState('');
     const [message, setMessage] = useState('');
     const navigate = useNavigate();
-    const location = useLocation();
-    const onUserAdded = location.state?.onUserAdded;
+    const { handleUserAdded } = useUser();
 
     const handleAddUser = async (event) => {
         event.preventDefault();
         try {
             const response = await axios.post('http://localhost:8080/api/auth/signup', {
-                name, email, password
+                name, email, password,section,shift
             });
             if (response.data) {
-                if (onUserAdded){
-                    onUserAdded(response.data);
-                }
+                handleUserAdded(response.data);
                 setName('');
                 setEmail('');
                 setPassword('');
+                setSection('');
+                setShift('');
                 setMessage('User added successfully!');
                 setTimeout(() => {
                     setMessage('');
@@ -66,6 +68,20 @@ const AddUserForm = () => {
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="section"
+                        placeholder="Section"
+                        value={section}
+                        onChange={(e) => setSection(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="shift"
+                        placeholder="Shift"
+                        value={shift}
+                        onChange={(e) => setShift(e.target.value)}
                         required
                     />
                     <div className="form-buttons">
