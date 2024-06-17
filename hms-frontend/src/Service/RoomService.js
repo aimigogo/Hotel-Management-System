f143b15ddcce4ba548aa637f0e8194e8c57bf41c
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:37:53 +0300

    Add the correct Api call

--- hms-frontend/src/Service/RoomService.js
+++ hms-frontend/src/Service/RoomService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-export const listRooms=()=> axios.get('http://localhost:8080/api/auth/rooms');
+export const listRooms=()=> axios.get('http://localhost:8080/api/room/rooms');
 
-export const updateRoom = (roomId, roomData) => axios.put(`http://localhost:8080/api/admin/rooms/${roomId}`, roomData);
+export const updateRoom = (roomId, roomData) => axios.put(`http://localhost:8080/api/room/update/${roomId}`, roomData);
 
-export const deleteRoom = (roomId) => axios.delete(`http://localhost:8080/api/admin/rooms/${roomId}`);
+export const deleteRoom = (roomId) => axios.delete(`http://localhost:8080/api/room/delete/${roomId}`);
