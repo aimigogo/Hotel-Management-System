c3ec31e17c3069452b67d08ba9a11371d867e9c6
aimigogo <aimiliagogolou5@gmail.com>
2024-06-17 17:38:29 +0300

    Added useLocation

--- hms-frontend/src/components/Sidebar.js
+++ hms-frontend/src/components/Sidebar.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Link, useLocation} from "react-router-dom";
 import '../css/Sidebar.css';
 import { ReactComponent as Logo } from '../assets/Logo.svg';
 import {ReactComponent as DashboardIcon } from '../assets/Inactive/dashboard.svg';
@@ -8,17 +8,29 @@ import {ReactComponent as RoomIcon } from '../assets/Inactive/hotel.svg';
 import {ReactComponent as RoomIconActive } from '../assets/Active/hotel.svg';
 import {ReactComponent as StockIcon } from '../assets/Inactive/store.svg';
 import {ReactComponent as StockIconActive } from '../assets/Active/store.svg';
-import {ReactComponent as SettingsIcon } from '../assets/Inactive/settings.svg';
-import {ReactComponent as SettingsIconActive } from '../assets/Active/settings.svg';
 import {ReactComponent as ExitIcon } from '../assets/Inactive/exit_to_app.svg';
 import {ReactComponent as ExitIconActive } from '../assets/Active/exit_to_app.svg';
-import {useUser} from "../context/UserRoleContext";
+import {useUser} from "../context/UserContext";
 
 
 
 export default function Sidebar() {
     const [activeItem,setActiveItem]=useState('dashboard');
     const {userType}=useUser();
+    const location=useLocation();
+
+    useEffect(() => {
+        const path = location.pathname;
+        if (path.includes('AdminDashboard') || path.includes('EmployeeDashboard')) {
+            setActiveItem('dashboard');
+        } else if (path.includes('RoomPage')) {
+            setActiveItem('rooms');
+        } else if (path.includes('StockPage')) {
+            setActiveItem('stock');
+        } else if (path.includes('Login')) {
+            setActiveItem('exit');
+        }
+    }, [location.pathname]);
 
     const handleItemClick=(item)=>{
         setActiveItem(item);
@@ -52,14 +64,6 @@ export default function Sidebar() {
                         Stock
                 </Link>
             </li>
-                <li className={`sidebar-item ${activeItem === 'settings'? 'active':''}`}
-                    onClick={()=>handleItemClick('settings')}>
-                    <Link to="/SettingsPage">
-                        {activeItem === 'settings' ? <SettingsIconActive className="sidebar-icon" />
-                            : <SettingsIcon className="sidebar-icon" />}
-                        Settings
-                    </Link>
-                </li>
                 <li className={`sidebar-item ${activeItem === 'exit'? 'active':''}`}
                     onClick={()=>handleItemClick('exit')}>
                     <Link to="/Login">
